8ac1460872b0739cc9f47c64f15c06cb50411b38
fTygerr <hcampa.crypto@gmail.com>
2024-04-28 04:16:49 -0400

    first commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="column">
+        <h1>About</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="column">
+        <p>This is about my mediocre site!!</p>
+    </div>
+</div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="column">
+        <h1>Contact</h1>
+    </div>
+</div>
+<div class="row">
+    <div class="column">
+        <p>Contact me at crypto@gmail.com</p>
+    </div>
+</div>
